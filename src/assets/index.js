934780e2a8cc98c98db50390c7bb1d285f7e1091
Abdul Hanan Nawaz <hanan-nawaz@Abduls-MacBook-Pro.local>
2024-08-02 20:28:26 +0500

    all files added

---
+++ src/assets/index.js
@@ -0,0 +1,33 @@
+import hanan from "./photo2.jpg";
+import backend from "./backend.png";
+import creator from "./creator.png";
+import mobile from "./mobile.png";
+import web from "./web.png";
+
+import github from "./github.png";
+import demo from "./demo.png"
+
+import git from "./tech/git.png";
+import mongodb from "./tech/mongodb.png";
+
+
+import clg from "./clg.jpg";
+import school from "./school.jpeg";
+import uni from "./uni.png";
+
+export {
+  backend,
+  creator,
+  mobile,
+  web,
+  github,
+  
+  git,
+  mongodb,
+  demo,
+  clg,
+  uni,
+  school,
+  hanan,
+
+};
